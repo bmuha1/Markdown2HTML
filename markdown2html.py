613216bf7ce8d54afcfc2a18292a136bcf0f3eae
Brent Muha <brent.muha@gmail.com>
2020-07-27 16:08:27 -0700

    Add start a script

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Take 2 strings as arguments.
+The first is the name of the Markdown file.
+The second is the output file name.
+'''
+
+import sys
+import os.path
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html')
+        exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]))
+        exit(1)
+    exit(0)
