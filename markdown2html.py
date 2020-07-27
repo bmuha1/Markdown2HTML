8078a486a2b625741f0d453b83b782771296c241
Brent Muha <brent.muha@gmail.com>
2020-07-27 16:12:22 -0700

    Print error messages to STDERR

--- markdown2html.py
+++ markdown2html.py
@@ -10,9 +10,10 @@ import os.path
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html')
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
         exit(1)
     if not os.path.isfile(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]))
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
     exit(0)
